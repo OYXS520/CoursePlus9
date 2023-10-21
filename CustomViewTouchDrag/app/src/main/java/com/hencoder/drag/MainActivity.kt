57d057eebd07b730ca3818ec327dffcac30a41f1
peanut <shuouyang0@gmail.com>
2023-10-21 16:12:40 +0800

    完成MotionLayout动画效果

--- CustomViewTouchDrag/app/src/main/java/com/hencoder/drag/MainActivity.kt
+++ CustomViewTouchDrag/app/src/main/java/com/hencoder/drag/MainActivity.kt
@@ -12,17 +12,17 @@ class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.drag_up_down)
-    collectLayout.setOnDragListener{v,event ->
-      when (event.action) {
-        DragEvent.ACTION_DROP -> if (v is LinearLayout) {
-          collectLayout.addView(TextView(this).apply {
-            text = event.clipData.getItemAt(0).text
-            textSize = 30f
-          })
-        }
-      }
-      true
-    }
+//    collectLayout.setOnDragListener{v,event ->
+//      when (event.action) {
+//        DragEvent.ACTION_DROP -> if (v is LinearLayout) {
+//          collectLayout.addView(TextView(this).apply {
+//            text = event.clipData.getItemAt(0).text
+//            textSize = 30f
+//          })
+//        }
+//      }
+//      true
+//    }
 
   }
 }
--- MotionLayout/transition/src/main/java/com/example/motionlayout/SampleActivity.kt
+++ MotionLayout/transition/src/main/java/com/example/motionlayout/SampleActivity.kt
@@ -2,6 +2,8 @@ package com.example.motionlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.constraintlayout.motion.widget.TransitionAdapter
 
 class SampleActivity : AppCompatActivity() {
 
